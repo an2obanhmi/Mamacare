4631bd8e7a139e185323c07771a0b49feb4906eb
Huyng <huypyke1102@gmail.com>
2024-11-20 20:39:28 +0700

    Thêm react-router-dom vào dependencies

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { UserProvider } from './components/UserContext';
+import { CartProvider } from './components/CartTemp/CartContext';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Services from './components/Services/Services';
+import AboutUs from './components/AboutUs/AboutUs';
+import Resource from './components/Resources/Resources';
+import Recruitment from './components/Recruitment/Recruitment';
+import ServiceDetail from './components/Services/ServiceDetail';
+import ProductDetail from './components/Product/ProductDetail';
+import Cart from './components/CartTemp/Cart'; // Cart Page Component
+import Infor from './components/Infor/Infor';
+import AuthPage from './components/Login/AuthPage';
+import RegisterPage from './components/Login/RegisterPage';
+import ProtectedRoute from './components/ProtectedRoute';
+import ServicePay from './components/Services/ServicePay';
+import Product from './components/Product/Product';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <UserProvider>
+            <CartProvider>
+                <Router>
+                    <div className="App">
+                        <Header />
+                        <Routes>
+                            <Route path="/login" element={<AuthPage />} />
+                            <Route path="/register" element={<RegisterPage />} />
+                            <Route path="/" element={<Home />} />
+                            <Route path="/services" element={<Services />} />
+                            <Route path="/about-us" element={<AboutUs />} />
+                            <Route path="/resources" element={<Resource />} />
+                            <Route path="/recruitment" element={<Recruitment />} />
+                            <Route path="/product" element={<Product />} />
+                            <Route path="/service-pay" element={<ServicePay />} />
+                            <Route path="/cart" element={<Cart />} /> {/* Cart page route */}
+
+                            {/* Protected route for service details */}
+                            <Route
+                                path="/service/:typeService"
+                                element={
+                                    <ProtectedRoute>
+                                        <ServiceDetail />
+                                    </ProtectedRoute>
+                                }
+                            />
+
+                            {/* Protected route for product details */}
+                            <Route path="/product/:productId" element={<ProductDetail />} />
+                        </Routes>
+                        <Infor />
+                        <Footer />
+                    </div>
+                </Router>
+            </CartProvider>
+        </UserProvider>
+    );
 }
 
 export default App;
